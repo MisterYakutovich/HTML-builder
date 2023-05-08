55ff65f9e64c5afed0c0336f73756270b0ebf734
Pavel <pashtet.net5@gmail.com>
2023-05-09 02:40:05 +0300

    feat:add result in folder 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs=require("fs")
+const path=require("path")
+const fsPromises = fs.promises;
 
+fsPromises.mkdir(path.join(__dirname,'files-copy')).then(function() {
+    console.log('Directory created successfully');
+}).catch(function() {
+    console.log('failed to create directory');
+});
+
+ fs.readdir('files',(err,data)=>{  
+    data.forEach(file=>{
+        console.log(file+" - "+path.extname(file)+" - "+fs.statSync("files/" +file).size+"B")    
+    }) 
+})
+
+//let { COPYFILE_EXCL } = fs.constants;
+fs.copyFile('files/test-css.css', 'files-copy/test-css.css', err => {
+   if(err) throw err; 
+   console.log('Файл успешно скопирован');
+});
+
+fs.copyFile('files/test-image.jpg', 'files-copy/test-image.jpg', err => {
+   if(err) throw err; 
+   console.log('Файл успешно скопирован');
+});
+fs.copyFile('files/test-js.js', 'files-copy/test-js.js', err => {
+    if(err) throw err;
+    console.log('Файл успешно скопирован');
+ });
+ fs.copyFile('files/test-text.txt', 'files-copy/test-text.txt', err => {
+    if(err) throw err; 
+    console.log('Файл успешно скопирован');
+ });
+ 
