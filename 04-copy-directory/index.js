fa8f4ad00f0e8b4a9957d588918b2482ecd8fb1f
Pavel <pashtet.net5@gmail.com>
2023-05-09 22:43:26 +0300

    fix:changed path to root directory,replaced synchronous function with asynchronous in folder 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,29 +8,29 @@ fsPromises.mkdir(path.join(__dirname,'files-copy')).then(function() {
     console.log('failed to create directory');
 });
 
- fs.readdir('files',(err,data)=>{  
+ fs.readdir(path.join(__dirname,'files'),(err,data)=>{  
     data.forEach(file=>{
-        fs.stat(path.join(__dirname,'secret-folder',file), function(err, stats) {               
+        fs.stat(path.join(__dirname,'files',file), function(err, stats) {               
             console.log(`${file} - ${path.extname(file)} -  ${stats.size}B`)  
     }) 
 })
  })
 
 //let { COPYFILE_EXCL } = fs.constants;
-fs.copyFile('files/test-css.css', 'files-copy/test-css.css', err => {
+fs.copyFile(path.join(__dirname,'files/test-css.css'), path.join(__dirname,'files-copy/test-css.css'), err => {
    if(err) throw err; 
    console.log('Файл успешно скопирован');
 });
 
-fs.copyFile('files/test-image.jpg', 'files-copy/test-image.jpg', err => {
+fs.copyFile(path.join(__dirname,'files/test-image.jpg'), path.join(__dirname,'files-copy/test-image.jpg'), err => {
    if(err) throw err; 
    console.log('Файл успешно скопирован');
 });
-fs.copyFile('files/test-js.js', 'files-copy/test-js.js', err => {
+fs.copyFile(path.join(__dirname,'files/test-js.js'), path.join(__dirname,'files-copy/test-js.js'), err => {
     if(err) throw err;
     console.log('Файл успешно скопирован');
  });
- fs.copyFile('files/test-text.txt', 'files-copy/test-text.txt', err => {
+ fs.copyFile(path.join(__dirname,'files/test-text.txt'), path.join(__dirname,'files-copy/test-text.txt'), err => {
     if(err) throw err; 
     console.log('Файл успешно скопирован');
  });
